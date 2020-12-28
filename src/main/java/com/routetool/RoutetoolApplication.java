39dbfa1a86169753b4af0f4d2a9a70cb2a010362
hefan798450087 <798450087@qq.com>
2020-12-28 19:08:05 +0800

    初始化工程

---
+++ src/main/java/com/routetool/RoutetoolApplication.java
@@ -0,0 +1,13 @@
+package com.routetool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutetoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoutetoolApplication.class, args);
+    }
+
+}
